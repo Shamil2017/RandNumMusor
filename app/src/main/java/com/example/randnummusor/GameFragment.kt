4c8a770f7a92a8a6a9da927f59d68e2a8abf09e5
OtsokovShA <shamil24@mail.ru>
2023-11-15 12:33:53 +0300

    Изменение типа randomNumber на статический

--- app/src/main/java/com/example/randnummusor/GameFragment.kt
+++ app/src/main/java/com/example/randnummusor/GameFragment.kt
@@ -1,10 +1,12 @@
 package com.example.randnummusor
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.findNavController
 import com.example.randnummusor.databinding.FragmentGameBinding
 
@@ -12,7 +14,10 @@ import com.example.randnummusor.databinding.FragmentGameBinding
 class GameFragment : Fragment() {
     private var _binding: FragmentGameBinding? = null
     private val binding get() = _binding!!
-    var randomNumber = 0
+    companion object {
+        var randomNumber = (1..100).random()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,7 +25,9 @@ class GameFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         val view = binding.root
-        randomNumber = (1..100).random()
+
+        Toast.makeText(getActivity(),randomNumber.toString(),Toast.LENGTH_SHORT).show()
+        Log.d("GameFragment",randomNumber.toString() )
 
         binding.btnRandNumber.setOnClickListener() {
             var userNumber: Int = binding.edNumber.text.toString().toInt()
